afa87c0e1d71acdaa3f231446f3b3d98c5dc9099
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 03:23:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,20 @@
 class Dog
- 
+
   def initialize(name, breed)
     @name = name
     @breed = breed
   end
- 
+
   def name=(name,breed)
     @name = name
     @breed = breed
   end
- 
+
   def name
     @name = name
   end
   def breed
     @breed = breed
   end
- 
+
 end
